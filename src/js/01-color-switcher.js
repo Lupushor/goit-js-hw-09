9d708b7084079953e81dd6325c98a492ce2fd826
Lupushor <alexandr.lupu.1984@gmail.com>
2023-03-16 18:08:11 +0100

    HW9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const changeColor = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+changeColor.stopBtn.disabled = true;
+let id = null;
+
+const onColorChange = () => {
+  id = setInterval(() => {
+    changeColor.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  changeColor.startBtn.disabled = true;
+  changeColor.stopBtn.disabled = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const offColorChange = () => {
+  clearInterval(id);
+  changeColor.stopBtn.disabled = true;
+  changeColor.startBtn.disabled = false;
+};
+
+changeColor.startBtn.addEventListener('click', onColorChange);
+changeColor.stopBtn.addEventListener('click', offColorChange);
